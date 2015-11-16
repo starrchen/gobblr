df70016e2507f1f34e9bd724b49c9707bf73bcbc
Starr Z. Chen <chenstarr@gmail.com>
2015-11-16 11:05:46 -0500

    Add description column to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116152744) do
+ActiveRecord::Schema.define(version: 20151116160442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,10 +36,11 @@ ActiveRecord::Schema.define(version: 20151116152744) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "photo"
     t.string   "url"
+    t.string   "description"
   end
 
   create_table "users", force: :cascade do |t|
