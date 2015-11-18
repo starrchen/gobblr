79a78c4a37fab347e04d8d6b391afb9f570115f0
Starr Z. Chen <chenstarr@gmail.com>
2015-11-18 16:05:51 -0500

    Fixed bug with recipe strong params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -56,7 +56,7 @@ class RecipesController < ApplicationController
 
   private
   def recipe_params
-    params.require(:recipe).permit(:name)
+    params.require(:recipe).permit(:name, :photo, :url, :description, :steps)
   end
 
 end
